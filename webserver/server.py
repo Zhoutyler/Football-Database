5924803da4739c2c5ec355390e4265b73a855ad4
scorpiobg <nengchen11@fudan.edu.cn>
2018-11-18 21:52:53 -0500

    fix bugs

--- webserver/server.py
+++ webserver/server.py
@@ -1,210 +1,186 @@
 #!/usr/bin/env python2.7
 
-"""
-Columbia's COMS W4111.001 Introduction to Databases
-Example Webserver
-
-To run locally:
-
-    python server.py
-
-Go to http://localhost:8111 in your browser.
-
-A debugger such as "pdb" may be helpful for debugging.
-Read about it online.
-"""
-
+import re
+import datetime
 import os
 from sqlalchemy import *
 from sqlalchemy.pool import NullPool
-from flask import Flask, request, render_template, g, redirect, Response
+from flask import Flask, request, abort, url_for, render_template, g, redirect, Response, session
+import json
+import datetime
 
 tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
 app = Flask(__name__, template_folder=tmpl_dir)
-
-
-#
-# The following is a dummy URI that does not connect to a valid database. You will need to modify it to connect to your Part 2 database in order to use the data.
-#
-# XXX: The URI should be in the format of: 
-#
-#     postgresql://USER:PASSWORD@104.196.18.7/w4111
-#
-# For example, if you had username biliris and password foobar, then the following line would be:
-#
-#     DATABASEURI = "postgresql://biliris:foobar@104.196.18.7/w4111"
-#
-DATABASEURI = "postgresql://user:password@104.196.18.7/w4111"
-
-
-#
-# This line creates a database engine that knows how to connect to the URI above.
-#
+DATABASEURI = "postgresql://nc2734:4291@35.196.158.126/proj1part2"
 engine = create_engine(DATABASEURI)
+app.secret_key = 'w4111proj1'
 
-#
-# Example of running queries in your database
-# Note that this will probably not work if you already have a table named 'test' in your database, containing meaningful data. This is only an example showing you how to run queries in your database using SQLAlchemy.
-#
-engine.execute("""CREATE TABLE IF NOT EXISTS test (
-  id serial,
-  name text
-);""")
-engine.execute("""INSERT INTO test(name) VALUES ('grace hopper'), ('alan turing'), ('ada lovelace');""")
+
+
+
+def getPlayerName(playerIDs):
+    ans = []
+    for playerid in playerIDs:
+        try:
+            pname = g.conn.execute('SELECT full_name FROM Player WHERE playerID=%s', playerid)
+            ans.append(pname.fetchall()[0][0])
+        except Exception as e:
+            print e
+    return ans
+
+def getPlayerInfo(playerName):
+    try:
+        pinfo = g.conn.execute('SELECT * FROM Player WHERE full_name=%s', playerName)
+        return pinfo.fetchall()
+    except Exception as e:
+        print e
+        return []
 
 
 @app.before_request
 def before_request():
-  """
-  This function is run at the beginning of every web request 
-  (every time you enter an address in the web browser).
-  We use it to setup a database connection that can be used throughout the request.
+    try:
+        g.conn = engine.connect()
+    except:
+        print("uh oh, problem connecting to database")
+        import traceback;
+        traceback.print_exc()
+        g.conn = None
 
-  The variable g is globally accessible.
-  """
-  try:
-    g.conn = engine.connect()
-  except:
-    print "uh oh, problem connecting to database"
-    import traceback; traceback.print_exc()
-    g.conn = None
 
 @app.teardown_request
 def teardown_request(exception):
-  """
-  At the end of the web request, this makes sure to close the database connection.
-  If you don't, the database could run out of memory!
-  """
-  try:
-    g.conn.close()
-  except Exception as e:
-    pass
+    try:
+        if session:
+          session.clear()
+        g.conn.close()
+    except Exception as e:
+        print "Exception when tear down:", e
+        pass
 
 
-#
-# @app.route is a decorator around index() that means:
-#   run index() whenever the user tries to access the "/" path using a GET request
-#
-# If you wanted the user to go to, for example, localhost:8111/foobar/ with POST or GET then you could use:
-#
-#       @app.route("/foobar/", methods=["POST", "GET"])
-#
-# PROTIP: (the trailing / in the path is important)
-# 
-# see for routing: http://flask.pocoo.org/docs/0.10/quickstart/#routing
-# see for decorators: http://simeonfranklin.com/blog/2012/jul/1/python-decorators-in-12-steps/
-#
 @app.route('/')
 def index():
-  """
-  request is a special object that Flask provides to access web request information:
-
-  request.method:   "GET" or "POST"
-  request.form:     if the browser submitted a form, this contains the data in the form
-  request.args:     dictionary of URL arguments, e.g., {a:1, b:2} for http://localhost?a=1&b=2
-
-  See its API: http://flask.pocoo.org/docs/0.10/api/#incoming-request-data
-  """
-
-  # DEBUG: this is debugging code to see what request looks like
-  print request.args
+    return render_template("index.html")
 
 
-  #
-  # example of a database query
-  #
-  cursor = g.conn.execute("SELECT name FROM test")
-  names = []
-  for result in cursor:
-    names.append(result['name'])  # can also be accessed using result[0]
-  cursor.close()
-
-  #
-  # Flask uses Jinja templates, which is an extension to HTML where you can
-  # pass data to a template and dynamically generate HTML based on the data
-  # (you can think of it as simple PHP)
-  # documentation: https://realpython.com/blog/python/primer-on-jinja-templating/
-  #
-  # You can see an example template in templates/index.html
-  #
-  # context are the variables that are passed to the template.
-  # for example, "data" key in the context variable defined below will be 
-  # accessible as a variable in index.html:
-  #
-  #     # will print: [u'grace hopper', u'alan turing', u'ada lovelace']
-  #     <div>{{data}}</div>
-  #     
-  #     # creates a <div> tag for each element in data
-  #     # will print: 
-  #     #
-  #     #   <div>grace hopper</div>
-  #     #   <div>alan turing</div>
-  #     #   <div>ada lovelace</div>
-  #     #
-  #     {% for n in data %}
-  #     <div>{{n}}</div>
-  #     {% endfor %}
-  #
-  context = dict(data = names)
-
-
-  #
-  # render_template looks in the templates/ folder for files.
-  # for example, the below file reads template/index.html
-  #
-  return render_template("index.html", **context)
-
-#
-# This is an example of a different path.  You can see it at:
-# 
-#     localhost:8111/another
-#
-# Notice that the function name is another() rather than index()
-# The functions for each app.route need to have different names
-#
-@app.route('/another')
-def another():
-  return render_template("another.html")
-
-
-# Example of adding new data to the database
-@app.route('/add', methods=['POST'])
-def add():
-  name = request.form['name']
-  g.conn.execute('INSERT INTO test VALUES (NULL, ?)', name)
-  return redirect('/')
+@app.route('/register')
+def getRegis():
+    return render_template("register.html")
 
 
 @app.route('/login')
-def login():
-    abort(401)
-    this_is_never_executed()
+def getLogin():
+    return render_template("login.html")
 
+@app.route('/register', methods=['POST'])
+def register():
+    thisname = request.form['usrname']
+    password = request.form['password']
+    try:
+        usernames = g.conn.execute('''SELECT A.username FROM Account A ''')
+        for unm in usernames:
+            if unm == thisname:
+                usernames.close()
+                return render_template('register.html', msg='Username exists!')
+        g.conn.execute('''INSERT INTO  Account (username, pswd) VALUES (%s, %s)''', (thisname, password))
+        usernames.close()
+        return render_template('register.html', msg='User:%s registered!'%usernames)
+    except Exception as e:
+        error = str(e)
+        print(error)
+    return render_template('register.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+    try:
+        username = request.form['username']
+        session['username'] = username
+        password = request.form['password']
+        session['password'] = password
+        uids = g.conn.execute('''SELECT userID FROM Account WHERE username = %s AND pswd = %s''', (username, password))
+        uid = uids.fetchone()
+        if uid:
+            session['uid'] = uid[0]
+            s = g.conn.execute('SELECT * FROM Favorites WHERE userID=%s', uid[0])
+            uids.close()
+            playerIDs = [p[0] for p in s.fetchall()]
+            return render_template("profile.html", u=[(uid[0], username)], f=getPlayerName(playerIDs))
+    except Exception as e:
+        error = str(e)
+        print(error)
+    return render_template('login.html')
+
+@app.route('/profile')
+def profile():
+    if 'uid' not in session:
+        return render_template('login.html')
+    try:
+        s = g.conn.execute('SELECT * FROM Favorites WHERE userID=%s', session['uid'])
+        playerIDs = [p[0] for p in s.fetchall()]
+        return render_template("profile.html", u=[(session['uid'], session['username'])], f=getPlayerName(playerIDs))
+    except Exception as e:
+        error = str(e)
+        print(error)
+    return render_template('login.html')
+
+
+
+
+@app.route('/playerName', methods=['POST'])
+def showPlayer():
+    playerName = request.form['player']
+    pinfo = getPlayerInfo(playerName)
+    if pinfo:
+        return render_template("playerInfo.html", p=pinfo)
+    return render_template("index.html")
+
+@app.route('/leagueMatch', methods=['POST'])
+def showMatch():
+    leagueName = request.form['leagueName']
+    try:
+        matchInfo = g.conn.execute('SELECT * FROM Match WHERE compName=%s', leagueName)
+        return render_template("leagueMatch.html", m=matchInfo)
+    except Exception as e:
+        error = str(e)
+        print(error)
+    return render_template("index.html")
+
+@app.route('/favourites')
+def addFav():
+    if "uid" not in session:
+        return render_template('login.html')
+    try:
+        players = g.conn.execute('SELECT * FROM Player')
+        s = g.conn.execute('SELECT playerID FROM Favorites WHERE userID=%s', session['uid'])
+        favoured = set([i[0] for i in s])
+        cands = []
+        for player in players:
+            if player[0] not in favoured:
+                cands.append(player)
+        return render_template('favourites.html', cands)
+
+    except Exception as e:
+        error = str(e)
+        print(error)
+        return render_template('index.html')
 
 if __name__ == "__main__":
-  import click
-
-  @click.command()
-  @click.option('--debug', is_flag=True)
-  @click.option('--threaded', is_flag=True)
-  @click.argument('HOST', default='0.0.0.0')
-  @click.argument('PORT', default=8111, type=int)
-  def run(debug, threaded, host, port):
-    """
-    This function handles command line parameters.
-    Run the server using:
-
-        python server.py
-
-    Show the help text using:
-
-        python server.py --help
-
-    """
-
-    HOST, PORT = host, port
-    print "running on %s:%d" % (HOST, PORT)
-    app.run(host=HOST, port=PORT, debug=debug, threaded=threaded)
+    import click
 
 
-  run()
+    @click.command()
+    @click.option('--debug', is_flag=True)
+    @click.option('--threaded', is_flag=True)
+    @click.argument('HOST', default='0.0.0.0')
+    @click.argument('PORT', default=8111, type=int)
+    def run(debug, threaded, host, port):
+        HOST, PORT = host, port
+        print("running on %s:%d" % (HOST, PORT))
+        app.run(host=HOST, port=PORT, debug=debug, threaded=threaded)
+
+
+    run()
+
+
+
